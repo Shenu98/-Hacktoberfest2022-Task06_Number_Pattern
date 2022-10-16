52ba9e51ee0f71add3b5f2c2af98adc9e690623b
Duvindu Chamod Ruwanara <110399244+Duvindu-Chamod-Ruwanara@users.noreply.github.com>
2022-10-16 13:15:36 +0530

    Task04-DONE

--- Main.java
+++ Main.java
@@ -1,29 +1,18 @@
 import java.util.Scanner;
-public class Main
-{
-     public static void main(String[] args)
-        {
-            Scanner sc = new Scanner(System.in);
- 
-            System.out.println("Enter the number of rows: ")
- 
-            int rows = sc.nextInt();
-             
-            for (int i=1; i<= rows ; i)
-            {
-                for (int j = i; j < rows ; j++) 
-                    System.out.print(" ");
-                }   
-                for (int k = 1; k <= (2*i -1) ;k++) {
-                    if( k==1 || i == rows || k==(2*i-1)) {
-                        System.out.print("*");
-                    }
-                    else {
-                        System.out.print(" ");
-                    }
-                }
-                System.out.println("")
-            }
-            sc.close();
-        }
-    }
+class Task4{
+     public static void main(String[] args) {
+     int i,j,k,row;
+     Scanner s = new Scanner(System.in);
+     System.out.print("Enter the number of rows: ");
+     row= s.nextInt();
+     for(i=1; i<=row; i++){
+          for(j=4; j>=i; j--){
+               System.out.print(" ");
+          }
+          for(k=1; k<=(2*i-1); k++){
+               System.out.print("*");
+          }
+     System.out.println("");
+     }
+     }
+}
